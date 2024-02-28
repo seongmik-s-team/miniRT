6c1c77bd38f6f6d1f36aa869202fc7167f241555
AhnJoonSung <tjdtna01@naver.com>
2024-02-28 16:57:36 +0900

    [LAST] Makefile header 추가 및 argc 에러 처리

--- srcs/minirt.c
+++ srcs/minirt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seongmik <seongmik@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jooahn <jooahn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 19:29:32 by seongmik          #+#    #+#             */
-/*   Updated: 2024/02/28 14:56:09 by seongmik         ###   ########.fr       */
+/*   Updated: 2024/02/28 16:56:05 by jooahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ int	main(int argc, char *argv[])
 {
 	t_scene	scene;
 
-	if (argc == 2)
-	{
-		scene_init(&scene);
-		parser(argv[1], &scene);
-		mr_mlx_init(&scene);
-		del_scene(&scene);
-	}
+	if (argc != 2)
+		pexit("Invalid argument's count");
+	scene_init(&scene);
+	parser(argv[1], &scene);
+	mr_mlx_init(&scene);
+	del_scene(&scene);
 	exit(SUCCESS);
 }
